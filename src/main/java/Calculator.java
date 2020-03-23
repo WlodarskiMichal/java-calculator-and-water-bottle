12dfbd849e25ceff375a5b4fbaeb41e3737d07da
howmehow <howmehow@gmail.com>
2020-03-23 18:38:05 +0000

    Initial Commit, Add method is tested and working.

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator {
+    private int x;
+    private int y;
+    public Calculator(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    public int add(){
+        int i = x + y;
+        return i;
+    };
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,13 @@
+import com.sun.deploy.net.MessageHeader;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+    @Test
+    public void canAdd(){
+        Calculator calculator = new Calculator(2, 2);
+        assertEquals(4, calculator.add());
+    }
+}
